8369b662eff535f9bd3bc5607c6a13754332373d
Imya <nomad@ag.ru>
2020-05-08 20:45:27 +0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+        try{
+            run();
+        }catch(Exception e){
+            e.getMessage();
+        }
+    }
+    static void run(){
+        System.out.println("Hello world");
+    }
+}
